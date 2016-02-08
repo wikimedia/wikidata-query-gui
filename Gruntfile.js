0ae3f46c92257b90c2f237eced0e37742f5409f7
Stanislav Malyshev <smalyshev@gmail.com>
2016-02-08 12:43:03 -0800

    Add test files
    Change-Id: I65166325856a2443a3c22cf2c7fb240151768c03

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'**/*.js',
+				'!vendor/**',
+                                '!node_modules/**',
+				'!**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		jsonlint: {
+			all: [
+				'*.json',
+				'**/*.json',
+                                '!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
