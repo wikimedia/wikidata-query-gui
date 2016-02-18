78c7355d960ae86feb677a70f0b5c94c01318e1a
jk <jonas.kress@wikimedia.de>
2016-02-18 10:34:25 +0100

    Image result browser
    Displays commons images from the query result as an masonry.
    
    Change-Id: I729680f72baca9b0344f006f3cea20275cc23aab

---
+++ wikibase/queryService/ui/resultBrowser/ImageResultBrowser.js
@@ -0,0 +1,127 @@
+var wikibase = wikibase || {};
+wikibase.queryService = wikibase.queryService || {};
+wikibase.queryService.ui = wikibase.queryService.ui || {};
+wikibase.queryService.ui.resultBrowser = wikibase.queryService.ui.resultBrowser || {};
+window.mediaWiki = window.mediaWiki || {};
+
+wikibase.queryService.ui.resultBrowser.ImageResultBrowser = ( function( $ ) {
+	"use strict";
+
+	var COMMONS_FILE_PATH = "http://commons.wikimedia.org/wiki/Special:FilePath/";
+	var COMMONS_SPECIAL_RESIZE = "http://commons.wikimedia.org/wiki/Special:FilePath/";
+
+	/**
+	 * A result browser for images
+	 *
+	 * @class wikibase.queryService.ui.resultBrowser.ImageResultBrowser
+	 * @licence GNU GPL v2+
+	 *
+	 * @author Jonas Kress
+	 * @constructor
+	 *
+	 * @param {Object} result set
+	 */
+	function SELF() {
+	}
+
+	SELF.prototype = new wikibase.queryService.ui.resultBrowser.AbstractResultBrowser();
+
+	/**
+	 * @property {jQuery}
+	 * @private
+	 **/
+	SELF.prototype._grid = null;
+
+	/**
+	 * Draw browser to the given element
+	 * @param {jQuery} $element to draw at
+	 **/
+	SELF.prototype.draw = function( $element ) {
+		var self = this;
+
+		this._grid = $( '<div class="masonry">' );
+
+
+		$.each( this._result.results.bindings, function(){
+			$.each( this, function( key, field ){
+				if( self._isCommonsRessource( field.value ) ){
+					var url = field.value,
+						regEx = new RegExp( COMMONS_FILE_PATH, "ig" ),
+						fileName = decodeURIComponent( url.replace( regEx, '' ) );
+
+					self._grid.append( self._getItem( self._getThumbnail( url ), fileName ) );
+				}
+			} );
+		} );
+
+		$( $element ).html( this._grid );
+	};
+
+	/**
+	 * Checks whether the browser can draw the given result
+	 * @return {bool}
+	 **/
+	SELF.prototype.isDrawable = function() {
+
+		var result = this._result.results.bindings[0] || {},
+		isDrawable = false;
+
+		$.each( result, function( key, field ){
+
+			if( field.value.startsWith( COMMONS_FILE_PATH ) ){
+				isDrawable = true;
+				return false;
+			}
+		} );
+
+		return isDrawable;
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._getItem = function( url, title ) {
+
+		var triggerGallery = function(event) {
+			event.preventDefault();
+			$(this).ekkoLightbox( { 'scale_height' : true } );
+			return false;
+		},
+			heading = $( '<div>' ).text( title ),
+			image = $( '<a href="' + url +'" data-gallery="g">' )
+			.click( triggerGallery )
+			.attr( 'data-title',  title )
+			.append( $( '<img src="' + url +'"></div>' ) );
+
+		return $( '<div class="item">' ).append( heading, image );
+
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._isCommonsRessource = function( url ) {
+		return url.toLowerCase().startsWith( COMMONS_FILE_PATH.toLowerCase() );
+
+	};
+
+	/**
+	 * @private
+	 **/
+	SELF.prototype._getThumbnail = function( url, width ) {
+		if( !this._isCommonsRessource(url) ){
+			return url;
+		}
+		if( !width ){
+			width = 400;
+		}
+
+		var regEx = new RegExp( COMMONS_FILE_PATH, "ig" ),
+		fileName = url.replace( regEx, '' ),
+		thumbnail = COMMONS_SPECIAL_RESIZE + fileName + '?width=' + width;
+
+		return thumbnail;
+	};
+
+	return SELF;
+}( jQuery ) );
