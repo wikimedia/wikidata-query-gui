53866d68cceac4ed21f7cdcbd4c2349fa26405b7
jk <jonas.kress@wikimedia.de>
2016-05-04 12:30:30 +0200

    Textual representation of LIMIT
    Bug: T133658
    Change-Id: Ia359dc60a50a6244b42e1485e42d620da8ec6d1c

--- wikibase/tests/queryService/ui/visualEditor/VisualEditor.test.js
+++ wikibase/tests/queryService/ui/visualEditor/VisualEditor.test.js
@@ -8,31 +8,31 @@
 				name: 'Cat query',
 				sparqlIn: 'SELECT ?item ?itemLabel WHERE { ?item wdt:P31 wd:Q146 . SERVICE wikibase:label { bd:serviceParam wikibase:language "en" } }',
 				sparqlOut: 'SELECT ?item ?itemLabel WHERE {\n  ?item wdt:P31 wd:Q146.\n  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }\n}',
-				text: 'Find instance of cat  Show'
+				text: 'Find instance of cat  Show Limit'
 			},
 			{
 				name: 'Any cat query',
 				sparqlIn: 'SELECT ?item ?itemLabel WHERE { ?item wdt:P31* wd:Q146 . SERVICE wikibase:label { bd:serviceParam wikibase:language "en" } }',
 				sparqlOut: 'SELECT ?item ?itemLabel WHERE {\n  ?item wdt:P31* wd:Q146.\n  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }\n}',
-				text: 'Find  any instance of cat  Show'
+				text: 'Find  any instance of cat  Show Limit'
 			},
 			{
 				name: 'Subtype cat query',
 				sparqlIn: 'SELECT * WHERE {?c  p:P31/ps:P31 wd:Q146 .}',
 				sparqlOut: 'SELECT * WHERE { ?c (p:P31/ps:P31) wd:Q146. }',
-				text: 'Find instance of or subtype instance of cat  Show'
+				text: 'Find instance of or subtype instance of cat  Show Limit'
 			},
 			{
 				name: 'List of presidents with causes of death',
 				sparqlIn: 'SELECT ?h ?cause ?hl ?causel WHERE { ?h wdt:P39 wd:Q11696 . ?h wdt:P509 ?cause . OPTIONAL {    ?h rdfs:label ?hl filter (lang(?hl) = "en") . } OPTIONAL {   ?cause rdfs:label ?causel filter (lang(?causel) = "en").  }}',
 				sparqlOut: 'SELECT ?h ?cause ?hl ?causel WHERE {\n  ?h wdt:P39 wd:Q11696.\n  ?h wdt:P509 ?cause.\n  OPTIONAL {\n    ?h rdfs:label ?hl.\n    FILTER((LANG(?hl)) = \"en\")\n  }\n  OPTIONAL {\n    ?cause rdfs:label ?causel.\n    FILTER((LANG(?causel)) = \"en\")\n  }\n}',
-				text: 'Find position held President of the United States of America  Show cause of death'
+				text: 'Find position held President of the United States of America  Show cause of death Limit'
 			},
 			{
 				name: 'List of actors with pictures with year of birth and/or death',
 				sparqlIn: 'SELECT ?human ?humanLabel ?yob ?yod ?picture WHERE{ ?human wdt:P31 wd:Q5 ; wdt:P106 wd:Q33999 . ?human wdt:P18 ?picture . OPTIONAL { ?human wdt:P569 ?dob . ?human wdt:P570 ?dod }. BIND(YEAR(?dob) as ?yob) . BIND(YEAR(?dod) as ?yod) . SERVICE wikibase:label {  bd:serviceParam wikibase:language "en" . }}LIMIT 88',
 				sparqlOut: 'SELECT ?human ?humanLabel ?yob ?yod ?picture WHERE {\n  ?human wdt:P31 wd:Q5.\n  ?human wdt:P106 wd:Q33999.\n  ?human wdt:P18 ?picture.\n  OPTIONAL {\n    ?human wdt:P569 ?dob.\n    ?human wdt:P570 ?dod.\n  }\n  BIND(YEAR(?dob) AS ?yob)\n  BIND(YEAR(?dod) AS ?yod)\n  SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". }\n}\nLIMIT 88',
-				text: 'Find instance of cat  with occupation actor  Show image'
+				text: 'Find instance of cat  with occupation actor  Show image Limit 88'
 			}
 	];
 
