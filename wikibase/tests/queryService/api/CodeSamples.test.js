d1b7810053e233808e95c1c57dfa5171d254f5ec
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2017-10-23 18:05:57 +0200

    Add CodeSamples tests
    The tests consist of a query and the expected code in each language, all
    placed in a folder under wikibase/tests/queryService/api/code-examples/
    named after the test case. (This commit adds only a single test case,
    “simple”; later commits can add more.)
    
    A symbolic link is added so that the CodeSamples API can still load the
    samples when running from wikibase/tests/ instead of the repository
    root. Unfortunately this does not work in a browser, so the test is
    disabled if it’s running in a browser. (Firefox’ same-origin policy for
    the file:// protocol quite sensibly prohibits access to parent
    directories of the document; Chrome/Chromium does not allow scripts to
    access file:// URIs at all unless launched with
    --allow-file-access-from-files.)
    
    Change-Id: Ia751a6902c11abbe76d0f758ac838d37a152debf

---
+++ wikibase/tests/queryService/api/CodeSamples.test.js
@@ -0,0 +1,61 @@
+( function( $, QUnit, sinon, wb ) {
+	'use strict';
+
+	QUnit.module( 'wikibase.queryService.api.CodeSamples' );
+
+	var CodeSamples = wb.queryService.api.CodeSamples;
+
+	var tests = [
+		{
+			name: 'simple',
+			endpoint: 'http://sparql.example/endpoint',
+			root: 'http://sparql.example/',
+			index: 'http://sparql.example/index.html'
+		}
+	];
+
+	$.each( tests, function( index, test ) {
+		QUnit.test( test.name, function( assert ) {
+			if ( typeof window !== 'undefined' ) {
+				// cannot load code examples in browser
+				assert.expect( 0 );
+				return;
+			}
+			var done = assert.async();
+			$.get(
+				'queryService/api/code-examples/' + test.name + '/query.sparql',
+				function( query ) {
+					new CodeSamples( test.endpoint, test.root, test.index )
+						.getExamples( query )
+						.then( function( examples ) {
+							var promises = [];
+							$.each( examples, function( lang, data ) {
+								promises.push( $.get(
+									'queryService/api/code-examples/' + test.name + '/' + lang + '.txt',
+									function( expected ) {
+										assert.strictEqual( data.code, expected );
+									},
+									'text'
+								) );
+							} );
+							$.when.apply( $, promises ).then(
+								done,
+								function( e ) {
+									assert.ok( false, 'could not load expected code' );
+									done();
+								}
+							);
+						} ).fail( function() {
+							assert.ok( false, 'could not load code samples' );
+							done();
+						} );
+				},
+				'text'
+			).fail( function() {
+				assert.ok( false, 'could not load query' );
+				done();
+			} );
+		} );
+	} );
+
+}( jQuery, QUnit, sinon, wikibase ) );
