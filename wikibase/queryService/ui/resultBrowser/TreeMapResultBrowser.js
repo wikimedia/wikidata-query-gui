dcb792cb575f9e4368eaba2569ee612e155df2b9
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2022-12-21 17:16:59 +0100

    eslint: Enable and fix no-console
    Elsewhere in the code, we bypass that rule by using `window.console`
    instead of `console`, and you can probably argue about whether that’s
    good or not; but this clearly looks like a leftover debug log to me, so
    let’s just remove it.
    
    Change-Id: Ia50b57eb480fce14d9e2823da23d8416f1391d4c

--- wikibase/queryService/ui/resultBrowser/TreeMapResultBrowser.js
+++ wikibase/queryService/ui/resultBrowser/TreeMapResultBrowser.js
@@ -252,7 +252,6 @@ wikibase.queryService.ui.resultBrowser.TreeMapResultBrowser = ( function( $, d3,
 		childrenCells.exit().remove();
 
 		d3.select( 'select' ).on( 'change', function() {
-			console.log( 'select zoom(node)' );
 			treemap.value( value == 'size' ? size : count ).nodes( root );
 			zoom( node );
 		} );
